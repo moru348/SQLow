2b2c9a2d7de35701005d93972ef5c8fbaac2238f
moru3 <moru348@gmail.com>
2020-12-11 23:03:55 +0900

    update

--- src/main/java/me/moru3/sqlow/Table.java
+++ src/main/java/me/moru3/sqlow/Table.java
@@ -5,28 +5,38 @@ import org.jetbrains.annotations.NotNull;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class Table implements ITable {
-    private final Column[] columns;
+    private ArrayList<Column> columns = new ArrayList<>();
     private final String name;
 
-    public Table(@NotNull String name,@NotNull Column[] columns) {
+    public Table(@NotNull String name,@NotNull ArrayList<Column> columns) {
         this.columns = columns;
         this.name = name;
+    }
+
+    public Table(@NotNull String name) {
+        this.name = name;
+    }
+
+    public Table addColumn(@NotNull Column column) {
+        columns.add(column);
+        return this;
     }
 
     public String build(boolean force) throws IllegalArgumentException, NoPropertyException {
         StringBuilder result = new StringBuilder();
         result.append("CREATE TABLE ").append(force ? "" + name : "IF NOT EXISTS " + name).append(" (");
-        Column[] primaryKeys = Arrays.stream(columns).filter(Column::isPrimaryKey).toArray(Column[]::new);
-        Column[] autoIncrements = Arrays.stream(columns).filter(Column::isAutoIncrement).toArray(Column[]::new);
+        Column[] primaryKeys = columns.stream().filter(Column::isPrimaryKey).toArray(Column[]::new);
+        Column[] autoIncrements = columns.stream().filter(Column::isAutoIncrement).toArray(Column[]::new);
         if(autoIncrements.length>1) throw new IllegalArgumentException("The maximum number of AI that can be set is 1.");
         if(SQLow.getDatabaseType()==DatabaseType.SQLITE && primaryKeys.length>1 && autoIncrements.length>=1) throw new IllegalArgumentException("If you set AI, PK is limited to one.");
         StringJoiner columnList = new StringJoiner(", ");
         StringJoiner primaryKeyList = new StringJoiner(", ");
-        Arrays.stream(columns).map(Column::build).forEach(columnList::add);
+        columns.stream().map(Column::build).forEach(columnList::add);
         Arrays.stream(primaryKeys).map(i -> "\"" + i.getName() + "\"").forEach(primaryKeyList::add);
         if(SQLow.getDatabaseType()==DatabaseType.SQLITE) primaryKeys = new Column[]{};
         if(primaryKeys.length>=1) columnList.add("PRIMARY KEY (" + primaryKeyList + ")");
