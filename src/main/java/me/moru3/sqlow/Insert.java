b482979c1a4325e768442e3590655a80304996ad
moru3 <moru348@gmail.com>
2020-12-17 11:22:11 +0900

    update.

--- src/main/java/me/moru3/sqlow/Insert.java
+++ src/main/java/me/moru3/sqlow/Insert.java
@@ -3,7 +3,6 @@ package me.moru3.sqlow;
 import me.moru3.sqlow.exceptions.NoPropertyException;
 import me.moru3.sqlow.utils.ObjConv;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -11,9 +10,14 @@ import java.util.*;
 
 public class Insert extends ObjConv {
     private final String tableName;
-    private final Map<String, String> values = new HashMap<>();
+    private Map<String, String> values = new HashMap<>();
     public Insert(@NotNull String tableName) {
         this.tableName = tableName;
+    }
+
+    public Insert(@NotNull String tableName, @NotNull Map<String, String> defaultValues) {
+        this.tableName = tableName;
+        values = defaultValues;
     }
 
     public Insert addValue(DataType<?> type, String key, Object value) {
@@ -30,8 +34,7 @@ public class Insert extends ObjConv {
         return this;
     }
 
-    public String build(@Nullable IgnoreType ignoreType) {
-        if(ignoreType==null) ignoreType=IgnoreType.IGNORE;
+    public String build(boolean force) {
         StringJoiner keyJoiner = new StringJoiner(",");
         StringJoiner valueJoiner = new StringJoiner(",");
         this.values.forEach((key, value) -> {
@@ -39,46 +42,24 @@ public class Insert extends ObjConv {
             valueJoiner.add(value);
         });
         StringBuilder result = new StringBuilder();
-        result.append("INSERT").append(ignoreType==IgnoreType.IGNORE?"": SQLow.getDatabaseType()!=DatabaseType.SQLITE?" IGNORE":" OR IGNORE").append(" INTO ")
+        result.append("INSERT").append(force?"": SQLow.getDatabaseType()!=DatabaseType.SQLITE?" IGNORE":" OR IGNORE").append(" INTO ")
                 .append(tableName)
                 .append(" (").append(keyJoiner).append(")")
                 .append(" VALUES (")
                 .append(valueJoiner)
                 .append(")");
-        if(ignoreType!=IgnoreType.UPDATE) return new String(result);
-        StringJoiner updateJoiner = new StringJoiner(",");
-        if(SQLow.getDatabaseType()!=DatabaseType.SQLITE) {
-            result.append(" ON DUPLICATE KEY UPDATE ");
-            this.values.forEach((key, value) -> updateJoiner.add(key + "="  + value));
-            result.append(updateJoiner);
-        } else {
-            result.append(" ON CONFLICT DO UPDATE SET ");
-            this.values.forEach((key, value) -> updateJoiner.add(key + "="  + value));
-            result.append(updateJoiner);
-        }
         return new String(result);
     }
 
-    public String build() {
-        return build(IgnoreType.IGNORE);
-    }
-
-    public void send(IgnoreType ignoreType) throws IllegalArgumentException, NoPropertyException, SQLException {
+    public void send(boolean force) throws IllegalArgumentException, NoPropertyException, SQLException {
         if(SQLow.getConnection()==null) throw new NoPropertyException("No connection has been created with SQ Low (Connection).");
-        PreparedStatement ps = SQLow.getConnection().prepareStatement(build(ignoreType));
-        ps.executeUpdate();
-        ps.close();
-    }
-
-    public void send() throws IllegalArgumentException, NoPropertyException, SQLException {
-        if(SQLow.getConnection()==null) throw new NoPropertyException("No connection has been created with SQ Low (Connection).");
-        PreparedStatement ps = SQLow.getConnection().prepareStatement(build(IgnoreType.IGNORE));
+        PreparedStatement ps = SQLow.getConnection().prepareStatement(build(force));
         ps.executeUpdate();
         ps.close();
     }
 
     @Override
     public String toString() {
-        return build(IgnoreType.IGNORE);
+        return build(false);
     }
 }
---
+++ src/main/java/me/moru3/sqlow/Upsert.java
@@ -0,0 +1,79 @@
+package me.moru3.sqlow;
+
+import me.moru3.sqlow.exceptions.NoPropertyException;
+import me.moru3.sqlow.utils.ObjConv;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Upsert {
+    private final String tableName;
+    private List<String> keys;
+    private final Map<String, String> values = new HashMap<>();
+    public Upsert(@NotNull String tableName) {
+        this.tableName = tableName;
+    }
+
+    /**
+     * It is for SQLite and does not need to be used with other SQL.
+     * @param key Primary key or unique index.
+     * @return Upsert.
+     */
+    public Upsert addKey(String key) {
+        keys.add(key);
+        return this;
+    }
+
+    public Upsert addValue(DataType<?> type, String key, Object value) {
+        values.put(key, type.getConvM().apply(value));
+        return this;
+    }
+
+    public Upsert addValue(String key, Object value) {
+        if(value instanceof Select) {
+            values.put(key, ((Select) value).build());
+            return this;
+        }
+        values.put(key, ObjConv.ObjToType(value).getConvM().apply(value));
+        return this;
+    }
+
+    /**
+     * Converts UPSERT to SQL syntax and returns it.
+     * @return String
+     */
+    public String build() {
+        StringBuilder result = new StringBuilder();
+        result.append(new Insert(tableName, values));
+        StringJoiner updateJoiner = new StringJoiner(",");
+        StringJoiner keyJoiner = new StringJoiner(",");
+        keys.forEach(keyJoiner::add);
+        if(SQLow.getDatabaseType()!=DatabaseType.SQLITE) {
+            result.append(" ON DUPLICATE KEY UPDATE ");
+            this.values.forEach((key, value) -> updateJoiner.add(key + "="  + value));
+            result.append(updateJoiner);
+        } else {
+            result.append(" ON CONFLICT(").append(keyJoiner).append(") DO UPDATE SET ");
+            this.values.forEach((key, value) -> updateJoiner.add(key + "="  + value));
+            result.append(updateJoiner);
+        }
+        return new String(result);
+    }
+
+    public void send() throws IllegalArgumentException, NoPropertyException, SQLException {
+        if(SQLow.getConnection()==null) throw new NoPropertyException("No connection has been created with SQLow (Connection).");
+        PreparedStatement ps = SQLow.getConnection().prepareStatement(build());
+        ps.executeUpdate();
+        ps.close();
+    }
+
+    @Override
+    public String toString() {
+        return build();
+    }
+}
